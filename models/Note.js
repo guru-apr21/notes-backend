7ad5e16ac01c12add40249ac66fd528c9431dc76
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-09-04 16:41:01 +0530

    Added user model and made both to reference each other

--- models/Note.js
+++ models/Note.js
@@ -4,6 +4,10 @@ const noteSchema = new mongoose.Schema({
   content: { type: String, required: true, minlength: 5 },
   date: { type: Date, default: new Date(), required: true },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 noteSchema.set("toJSON", {
